435fc3d00930134139e6b5f944b867f4ed8cd70c
Eric J Gruber <eric@rumblestrut.com>
2016-08-28 20:43:50 -0500

    Add code comments for clarity

--- main.js
+++ main.js
@@ -7,10 +7,12 @@ var vm = new Vue({
     mortgage: "Mortgage amount",
   },
   methods: {
+    // Format the number for currency
     currencyFormat: function(num) {
       // As seen at https://blog.tompawlak.org/number-currency-formatting-javascript.
       return "$" + num.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,")
     },
+    // Calculate the profit from the sale
     calculateProfit: function(num) {
       var price = document.getElementById('price').value;
       var fee = document.getElementById('fee').value / 100;
